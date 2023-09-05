5b8dc99a2c3a602de173cbbd1c7fc2e32449a30d
Yuri018 <plktmn@gmail.com>
2023-09-05 21:07:07 +0200

    added homeWork 35 tests

---
+++ Lesson35/homeWork35/code/test/Main35Test.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Main35Test {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void compareTwoLists() {
+    }
+
+    @Test
+    void limitList() {
+    }
+}

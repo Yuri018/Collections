8e74d8a51886424cc23f8ed4cc5b4785c0e5623d
Yuri018 <plktmn@gmail.com>
2023-09-06 19:58:43 +0200

    added Lesson 36 Iterators, Linked-lists

---
+++ Lesson36/src/linkedLists/Main.java
@@ -0,0 +1,20 @@
+package linkedLists;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        List<String> list = new LinkedList<>();
+
+         /*
+    LINKEDLIST
+    конструкторы:
+    LinkedList()  - пустой список
+    LinkedList( Collection collection) -  список со всеми элементами коллекции
+                collection
+     */
+    }
+}
